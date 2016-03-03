c3cce5ed2e81457158f14f735fea6ec5b4faa548
badnom <kiel@badnom.net>
2016-03-03 13:32:10 +0900

    MetricsExport 수정

--- lamp-admin/src/main/java/lamp/admin/web/config/ScheduleConfig.java
+++ lamp-admin/src/main/java/lamp/admin/web/config/ScheduleConfig.java
@@ -1,11 +1,13 @@
 package lamp.admin.web.config;
 
 import lamp.admin.web.service.AgentScheduledCheckService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+@Slf4j
 @Configuration
 public class ScheduleConfig implements SchedulingConfigurer {
 
@@ -17,6 +19,7 @@ public class ScheduleConfig implements SchedulingConfigurer {
 
 	@Override public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
 		Long healthCheckPeriod = serverProperties.getHealthCheckPeriod();
+		log.info("healthCheckPeriod = {}", healthCheckPeriod);
 		if (healthCheckPeriod != null && healthCheckPeriod > 0) {
 			taskRegistrar.addFixedRateTask(() -> {
 				agentScheduledCheckService.checkHealth();
@@ -24,6 +27,7 @@ public class ScheduleConfig implements SchedulingConfigurer {
 		}
 
 		Long metricsCheckPeriod = serverProperties.getMetricsCheckPeriod();
+		log.info("metricsCheckPeriod = {}", metricsCheckPeriod);
 		if (metricsCheckPeriod != null && metricsCheckPeriod > 0) {
 			taskRegistrar.addFixedRateTask(() -> {
 				agentScheduledCheckService.collectMetrics();
