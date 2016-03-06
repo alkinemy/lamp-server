a0d3e40f0c73cc86509c34802326d6bd70724235
joke111 <alta.second@gmail.com>
2016-03-06 20:32:55 +0900

    새로 등록된 유저에게 ROLE_USER 권한 부여

--- lamp-admin/src/main/java/lamp/admin/web/service/UserRegisterService.java
+++ lamp-admin/src/main/java/lamp/admin/web/service/UserRegisterService.java
@@ -1,11 +1,14 @@
 package lamp.admin.web.service;
 
+import com.google.common.collect.Sets;
 import lamp.admin.core.base.exception.Exceptions;
 import lamp.admin.utils.assembler.SmartAssembler;
 import lamp.admin.web.AdminErrorCode;
+import lamp.admin.web.domain.Authority;
 import lamp.admin.web.domain.User;
 import lamp.admin.web.domain.UserRegisterForm;
 import lamp.admin.web.repository.UserRepository;
+import lamp.admin.web.security.AuthoritiesConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -30,8 +33,13 @@ public class UserRegisterService {
 		userRepository.findOneByEmail(form.getEmail()).ifPresent(user -> {
 			throw Exceptions.newException(AdminErrorCode.DUPLICATED_USER_EMAIL);
 		});
+
+		Authority userAuthority = new Authority();
+		userAuthority.setName(AuthoritiesConstants.USER);
+
 		User user = assembler.assemble(form, User.class);
 		user.setPassword(passwordEncoder.encode(form.getPassword()));
+		user.setAuthorities(Sets.newHashSet(userAuthority));
 		return userRepository.save(user);
 	}
 }
