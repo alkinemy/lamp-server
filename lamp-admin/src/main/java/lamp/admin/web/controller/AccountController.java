e64d01295b19bf5f681e143bd2a357f9e1cf9cd2
joke111 <alta.second@gmail.com>
2016-03-06 22:04:54 +0900

    메뉴 맵핑 수정

--- lamp-admin/src/main/java/lamp/admin/web/controller/AccountController.java
+++ lamp-admin/src/main/java/lamp/admin/web/controller/AccountController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@MenuMapping(MenuConstants.ACCOUNT)
+@MenuMapping(MenuConstants.ACCOUNT_APPROVAL)
 @Controller
 @RequestMapping("/account")
 public class AccountController {
