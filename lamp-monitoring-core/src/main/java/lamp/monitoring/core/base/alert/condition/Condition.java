60f70b56206e52dae29ebe4cc82865e4fb8f661f
badnom <kiel@badnom.net>
2016-07-17 09:48:30 +0900

    구조 변경 ㅠㅠ

---
+++ lamp-monitoring-core/src/main/java/lamp/monitoring/core/base/alert/condition/Condition.java
@@ -0,0 +1,12 @@
+package lamp.monitoring.core.base.alert.condition;
+
+
+public class Condition {
+
+    // Evaluation
+    private ConditionOperator operator;
+
+    private Object key;
+    private Object value;
+
+}
---
+++ lamp-monitoring-core/src/main/java/lamp/monitoring/core/base/alert/condition/ConditionOperator.java
@@ -0,0 +1,7 @@
+package lamp.monitoring.core.base.alert.condition;
+
+/**
+ * Created by kangwoo on 7/17/16.
+ */
+public class ConditionOperator {
+}
