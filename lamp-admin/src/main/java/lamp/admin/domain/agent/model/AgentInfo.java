9569b4c5b449a18a50d3a1bcbca594c7f7e09257
joke111 <alta.second@gmail.com>
2016-07-18 18:15:23 +0900

    호스트 검색 로직 수정중

---
+++ lamp-admin/src/main/java/lamp/admin/domain/agent/model/AgentInfo.java
@@ -0,0 +1,14 @@
+package lamp.admin.domain.agent.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AgentInfo {
+
+	private String id;
+	private String artifactId;
+	//TODO 정보들 추가하기
+
+}
